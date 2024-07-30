b8a3be1397df891592c99b154e53a2aa3c6dbc95
HFeeney <haydenbfeeney@gmail.com>
2024-07-29 20:44:51 -0700

    Implement getAction.

--- PlayerController.cc
+++ PlayerController.cc
@@ -1,5 +1,23 @@
 #include "PlayerController.hh"
+#include "curses.h"
 
 Action PlayerController::getAction(unsigned int maxTime) {
-	// TODO
+    halfdelay(maxTime / 100); // Note: half delay accepts tenths of a second
+    int input = getch();
+
+    switch (input) {
+        case KEY_LEFT:
+            return LEFT;
+        case KEY_RIGHT:
+            return RIGHT;
+        case KEY_DOWN:
+            return DOWN;
+        case (int) 'd':
+            return ROTATE_LEFT;
+        case (int) 'f':
+            return ROTATE_RIGHT;
+        case ERR:
+        default:
+            return NONE;
+    }
 }
